cf40f94f29d694a21dcc3f0481c5e8c9f0465a43
Ecstatic7 <nima.nilchian@gmail.com>
2021-05-09 09:41:51 +0330

    creating repo

---
+++ cmake-build-debug/CMakeFiles/miniProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniProject.dir/main.cpp.obj"
+  "libminiProject.dll.a"
+  "miniProject.exe"
+  "miniProject.exe.manifest"
+  "miniProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miniProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
